d239cf485258e0ba56a4e8c9863d6273a4ae3aa5
Tristan Farneau <tfarneau@gmail.com>
2015-09-09 09:43:43 +0200

    home

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Hey</title>
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300italic,500italic,500,700,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300italic,500italic,500,700,900,900italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
@@ -11,8 +11,25 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
-<body>
-	<h1>Homepage</h1>
+<body class="l-home">
+	
+	<header class="c-header">
+		
+	</header>
+	
+	<section class="c-hero">
+		<div class="c-hero__content">
+			<h1>Deltaplane</h1>
+			<h2>The best app for simply manage your freelance business</h2>
+		</div>
+		<div class="c-hero__screen">
+			
+		</div>
+	</section>
+
+	<section class="c-content">
+		
+	</section>
 
 	<script src="public/app.js"></script>
 
